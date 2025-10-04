ac585d5285b711b0a6bb0c998e3d5f93a74fac58
vednav9 <vedantnavthale222@nhitm.ac.in>
2025-10-04 11:14:09 +0530

    Connect to DB

--- server/index.js
+++ server/index.js
@@ -1,5 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGODB)
+.then(()=> {
+    console.log('Connected to MONGO DB!');
+})
+.catch((err)=>{
+    console.log(err);
+});
 
 const app = express();
 
