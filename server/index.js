2cf92fceb70280546e7113eddcf3a69848a60613
vednav9 <vedantnavthale222@nhitm.ac.in>
2025-10-04 17:12:31 +0530

    create a middleware fucntion to handle possible error

--- server/index.js
+++ server/index.js
@@ -29,6 +29,17 @@ app.get('/', (req, res) => {
 app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
 
+// Middleware
+app.use((err,req,res,next)=>{
+  const statusCode=err.statusCode || 500;
+  const message=err.message||'Internal Server Error';
+  return res.status(statusCode).json({
+    success:false,
+    statusCode:statusCode,
+    message,
+  });
+});
+
 // Start server
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
