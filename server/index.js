860706efc8f0dc5e5c2254abf2612c4a9675bd31
vednav9 <vedantnavthale222@nhitm.ac.in>
2025-10-04 10:53:48 +0530

    Add cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
 import express from 'express';
-const app=express();
-app.listen(3000, ()=>{
-    console.log('Server is running on port 3000');
-}
-);
+import cors from 'cors';
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Sample route (optional)
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
